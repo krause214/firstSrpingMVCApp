7b566a4f6f37734771d17b81503ef646080e647c
Andrey <andron-2102@mail.ru>
2022-07-27 21:14:57 +0400

    @RequestMapping, html files

---
+++ src/main/java/my/babincev/spring/controllers/SecondController.java
@@ -0,0 +1,12 @@
+package my.babincev.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+    @GetMapping("/exit")
+    public String exit(){
+        return "second/exit";
+    }
+}
